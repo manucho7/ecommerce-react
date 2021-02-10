c9f2abb4a367dae6607a849243d1257940166065
manucho <manuel.blanco77@gmail.com>
2021-02-10 14:10:34 -0300

    Navbar component + Navbar styles + Products component fixes + products styles + Importation fix in App.js via /components/index

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Product from './Product/Product';
+import useStyles from './productStyles';
 
 const products = [
     {id: 1, name: 'Running Shoes', description: 'nike air max pro', price: '$5', image: 'https://static01.nyt.com/images/2020/02/25/well/PHYSED-SHOES1/merlin_168154896_a69879b8-d43a-40d0-8297-dd7086d7d784-jumbo.jpg?quality=90&auto=webp' },
@@ -8,8 +9,12 @@ const products = [
 ];
 
 const Products = () => {
+
+    const classes = useStyles();
+
     return (
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
             <Grid container justify="center" spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
