f315682bece6e10ba00baf111f411a5857957ded
manucho <manuel.blanco77@gmail.com>
2021-02-11 14:30:08 -0300

    Cart component structure + CartItem structure + cartItemStyles

--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -3,13 +3,26 @@ import { Typography, Button, Card, CardActions, CardContent, CardMedia } from '@
 
 import useStyles from './cartItemStyles';
 
-const CartItem = () => {
+const CartItem = ({ item }) => {
     const classes = useStyles();
-    
+
     return (
-        <div>
+        <Card>
+            <CardMedia image={item.media.source} alt={item.name} className={classes.media} />
+            <CardContent className={classes.cardContent}>
+                <Typography variant="h4">{item.name}</Typography>
+                <Typography variant="h5">{item.line_total.formatted_with_symbol}</Typography>
+            </CardContent>
             
-        </div>
+            <CardActions className={classes.cartActions}>
+                <div className={classes.buttons}>
+                    <Button type="button" size="small">-</Button>
+                    <Typography>{item.quantity}</Typography>
+                    <Button type="button" size="small">+</Button>
+                </div>
+                <Button variant="contained" type="button" color="secondary">Remove</Button>
+            </CardActions>
+        </Card>
     )
 }
 
