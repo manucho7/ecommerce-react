1ace6bc483ce8529a44409eaa47fe3b73539abc1
manucho <manuel.blanco77@gmail.com>
2021-02-11 14:14:26 -0300

    Cart component + cartStyles + CartItem component + cartItemStyles

---
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Typography, Button, Card, CardActions, CardContent, CardMedia } from '@material-ui/core';
+
+import useStyles from './cartItemStyles';
+
+const CartItem = () => {
+    const classes = useStyles();
+    
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CartItem
