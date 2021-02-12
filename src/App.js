ea795bfa8be96ffeb5c6fb810d9d442c5d62e930
manucho <manuel.blanco77@gmail.com>
2021-02-12 12:18:26 -0300

    Prop-drilling from App component to Cart and CartItem components, sending functionality to update, remove and empty cart

--- src/App.js
+++ src/App.js
@@ -11,15 +11,30 @@ const App = () => {
     const fetchProducts = async() => {
         const { data } = await commerce.products.list();
         setProducts( data );
-    }
+    };
 
     const fetchCart = async() => {
         setCart(await commerce.cart.retrieve());
-    }
+    };
 
     const handleAddToCart = async(productId, quantity) => {
         const item = await commerce.cart.add(productId, quantity);
         setCart(item.cart);
+    };
+
+    const handleUpdateCartQty = async(productId, quantity) => {
+        const response = await commerce.cart.update(productId, {quantity});
+        setCart(response.cart);
+    };
+
+    const handleRemoveFromCart = async(productId) => {
+        const response = await commerce.cart.remove(productId);
+        setCart(response.cart);
+    };
+
+    const handleEmptyCart = async() => {
+        const response = await commerce.cart.empty();
+        setCart(response.cart);
     }
 
     useEffect(() => {
@@ -31,7 +46,7 @@ const App = () => {
 
     return (
         <Router>
-            <div>
+            <div style={{ display: 'flex' }}>
                 <Navbar totalItems={cart.total_items}/>
                 <Switch>
                     <Route exact path="/">
@@ -39,7 +54,12 @@ const App = () => {
                     </Route>
 
                     <Route exact path="/cart">
-                        <Cart cart={cart} />
+                        <Cart 
+                            cart={cart}
+                            handleUpdateCartQty={handleUpdateCartQty}
+                            handleRemoveFromCart={handleRemoveFromCart}
+                            handleEmptyCart={handleEmptyCart}
+                        />
                     </Route>
                 </Switch>
             </div>
