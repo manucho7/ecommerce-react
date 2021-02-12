d117dfcdbde3ba5c44ff3a2152b4db32329cc856
manucho <manuel.blanco77@gmail.com>
2021-02-12 10:33:25 -0300

    Navbar cart icon shown only when were on homepage, hidden when we are in /cart route

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import {AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography} from '@material-ui/core';
-import {ShoppingCart} from '@material-ui/icons';
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
+import { ShoppingCart } from '@material-ui/icons';
+import { Link, useLocation } from 'react-router-dom';
 
 import useStyles from './navbarStyles';
 import logo from '../../assets/virtualCommerce.png';
@@ -8,23 +9,28 @@ import logo from '../../assets/virtualCommerce.png';
 const Navbar = ({ totalItems }) => {
 
     const classes = useStyles();
+    const location = useLocation();
+
+
 
     return (
         <>
             <AppBar position="fixed" className={classes.appBar} color="inherit">
                 <Toolbar>
-                    <Typography variant="h6" className={classes.title} color="inherit">
+                    <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
                         <img src={logo} alt="Virtual Commerce" height="25px" className={classes.image} />
                         Virtual Commerce
                     </Typography>
                     <div className={classes.grow} />
+                    {/* Only if were on home route, will this cart badge be visible*/}
+                    {location.pathname === '/' && (
                     <div className={classes.button} >
-                        <IconButton aria-label="Show cart items" color="inherit">
+                        <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
                             <Badge badgeContent={totalItems} color="secondary">
                                 <ShoppingCart />
                             </Badge>
                         </IconButton>
-                    </div>
+                    </div> )}
                 </Toolbar>
             </AppBar>
         </>
