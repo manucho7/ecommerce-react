0c7ca1eccd0028a7a77638c61d209f7f7f7708b1
manucho <manuel.blanco77@gmail.com>
2021-02-12 10:41:41 -0300

    Link to home from cart route, to add more items to cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Container, Typography, Button, Grid } from '@material-ui/core';
 import CartItem from './CartItem/CartItem';
+import { Link } from 'react-router-dom';
 import useStyles from './cartStyles';
 
 const Cart = ({ cart }) => {
     const classes = useStyles();
 
     const EmptyCart = () => (
-        <Typography variant="subtitle1">You have no items in your shopping cart, start adding some !</Typography>
+        <Typography variant="subtitle1">You have no items in your shopping cart, 
+            <Link to="/" className={classes.link}>start adding some !</Link>
+        </Typography>
     );
 
     const FilledCart = () => (
