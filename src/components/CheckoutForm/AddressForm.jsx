0e5cb56a09d4eb0ef36e4655f51495de3b09993f
manucho <manuel.blanco77@gmail.com>
2021-02-14 11:33:34 -0300

    Fetching countries in AddressForm component

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,11 +1,75 @@
-import React from 'react';
+import React,{ useState, useEffect } from 'react';
+import {InputLabel, SelectMenuItem, Button, Grid, Typography, Select, MenuItem} from '@material-ui/core';
+import { useForm, FormProvider } from 'react-hook-form';
+import FormInput from './FormInput';
 
-const AddressForm = () => {
+import { commerce } from '../../lib/commerce';
+
+const AddressForm = ({ checkoutToken }) => {
+    const [shippingCountries, setShippingCountries] = useState([]);
+    const [shippingCountry, setShippingCountry] = useState('');
+    const [shippingSubdivisions, setShippingSubdivisions] = useState([]);
+    const [shippingSubdivision, setShippingSubdivision] = useState('');
+    const [shippingOptions, setShippingOptions] = useState([]);
+    const [shippingOption, setShippingOption] = useState('');
+
+    const methods = useForm();
+
+    const countries = Object.entries(shippingCountries).map(([code, name]) => ({ id: code, label: name }))
+
+    const fetchShippingCountries = async(checkoutTokenId) => {
+        const { countries } = await commerce.services.localeListShippingContries(checkoutTokenId);
+        setShippingCountries( countries );
+        setShippingCountry(Object.keys(countries)[0]);
+    }
+
+    useEffect(() => {
+        fetchShippingCountries(checkoutToken.id)
+    }, []);
+
     return (
-        <div>
-            Address Form
-        </div>
-    )
-}
+        <>
+            <Typography variant="h6" gutterBottom>Shipping Address</Typography>
+            <FormProvider {...methods}>
+                <form onSubmit=''>
+                    <Grid container spacing={3}>
+                        <FormInput required name="firstName" label="First Name"/>
+                        <FormInput required name="lastName" label="Last Name"/>
+                        <FormInput required name="address1" label="Address"/>
+                        <FormInput required name="email" label="Email"/>
+                        <FormInput required name="City" label="City"/>
+                        <FormInput required name="ZIP" label="ZIP / Postal code"/>
+                        <Grid item xs={12} sm={6}>
+                            <InputLabel>Shipping Country</InputLabel>
+                            <Select value={shippingCountry} fullWidth onChange={(e) => setShippingCountry(e.target.value)}>
+                            {countries.map((country) => (
+                                <MenuItem key={country.id} value={country.id}>
+                                    {country.label}
+                                </MenuItem>
+                            ))}
+                            </Select>
+                        </Grid>
+                        {/*<Grid item xs={12} sm={6}>
+                            <InputLabel>Shipping Subdivision</InputLabel>
+                            <Select value={} fullWidth onChange={}>
+                                <MenuItem key={} value={}>
+                                    Select Me
+                                </MenuItem>
+                            </Select>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <InputLabel>Shipping Options</InputLabel>
+                            <Select value={} fullWidth onChange={}>
+                                <MenuItem key={} value={}>
+                                    Select Me
+                                </MenuItem>
+                            </Select>
+                        </Grid> */}
+                    </Grid>
+                </form>
+            </FormProvider>
+        </>
+    );
+};
 
 export default AddressForm
